c216d650d8f130799b926f70a91e2200cc58317b
Harish <beastmode70919@gmail.com>
2023-08-15 12:02:50 +0530

    CustomerPersonalDetails taken as input and stored to DB

---
+++ fundNest/src/main/java/com/app/dto/SignUpResponseDTO.java
@@ -0,0 +1,30 @@
+package com.app.dto;
+
+import java.util.List;
+
+import com.app.entities.CustomerKYCDetails;
+import com.app.entities.CustomerNomineeDetails;
+import com.app.entities.CustomerPersonalDetails;
+import com.app.entities.CustomerWalletTransaction;
+import com.app.entities.MFCompanyDetails;
+import com.app.entities.UserPortfolio;
+import com.app.entities.UserType;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class SignUpResponseDTO {
+
+	private String emailId;
+	private String password;
+	private String mobileNo;
+	private UserType userType;
+}
