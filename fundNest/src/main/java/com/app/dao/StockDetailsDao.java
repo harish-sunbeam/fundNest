61f06fa811165b1be6d54708cb077c559f405de8
sharvarishparulekar18@gmail.com <SHARVARISH>
2023-08-17 13:59:49 +0530

    deleted previous code

--- fundNest/src/main/java/com/app/dao/StockDetailsDao.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.entities.StockDetails;
-
-public interface StockDetailsDao extends JpaRepository<StockDetails, Long> {
-
-}
