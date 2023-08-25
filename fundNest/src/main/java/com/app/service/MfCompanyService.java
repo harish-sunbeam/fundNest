b4b718bea950b16bf40bbb9207b4b96c349670b4
Harish <beastmode70919@gmail.com>
2023-08-25 12:46:13 +0530

    getMfCompanyDetailsByCustId method added

--- fundNest/src/main/java/com/app/service/MfCompanyService.java
+++ fundNest/src/main/java/com/app/service/MfCompanyService.java
@@ -2,9 +2,12 @@ package com.app.service;
 
 import com.app.dto.MFCompanyProfileRequestDTO;
 import com.app.dto.MFProfileCompanyResponseDTO;
+import com.app.entities.MFCompanyDetails;
 
 public interface MfCompanyService {
 
 	MFProfileCompanyResponseDTO mfCompanyProfile(MFCompanyProfileRequestDTO requestDTO,Long custId);
+
+	MFProfileCompanyResponseDTO getMfCompanyDetailsByCustId(Long custId);
 	
 }
